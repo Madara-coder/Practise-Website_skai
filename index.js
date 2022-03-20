63a61252b59e2efe22d3b7b3975ec8990dc8a464
Debrath Sharma <debrathss@gmail.com>
2022-03-20 15:36:18 +0545

    First Version

---
+++ index.js
@@ -0,0 +1,11 @@
+// This is the js section for the website of skai made by Mr.Debrath Sharma...
+
+// Audio section of the javaScript
+//To use setTimeout() or syntax of hte setTimeout()
+setTimeout(function() {
+    const audio = document.getElementById("sound");
+    audio.play();
+}, 1 * 1000); // where 1000 is 1 second   the sound plays after 1 second
+
+// This is the section to check the console area by thanking hahahahha
+console.log("!!Thank You!!");
